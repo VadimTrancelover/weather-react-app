223946ce20c2ade1e4c8e300e84f5927d8da1b92
Vadim <fable919@gmail.com>
2022-02-18 00:08:41 +0300

    final first itteration

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const accordion = document.querySelectorAll('.accordion_forecast-weather')[0];
-
-
-accordion.addEventListener('click', () => {
-    document.querySelector('.accordion-forecast-weather__content').classList.toggle('is-open');
-    document.querySelector('.arrow').classList.toggle('rotated');
-})
